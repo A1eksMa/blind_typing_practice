c7b7fe0316c0d3ec8504b24523f8ec562bc56e82
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-06 03:41:26 +0600

    Update btp.py

--- btp.py
+++ btp.py
@@ -96,11 +96,25 @@ def main(stdscr):
     def main_window():
         stdscr.clear()
         curses.curs_set(True)
+
+        main_window = curses.newwin(max_height,
+                                  max_width,
+                                  0,
+                                  0)
+
+        main_window.bkgd(' ', curses.color_pair(2))
+
         # Draw rectangle frame
         rectangle_height = max_height - 2
         rectangle_width = max_width - 2
         rectangle(main_window, 1, 1, rectangle_height, rectangle_width)
-        stdscr.addstr('Blind Typing Trainer'.center(max_width))
+        
+        title = 'Blind Typing Trainer'
+        main_window.addstr(0,
+                         0,
+                         title.center(max_width),
+                         curses.color_pair(2))
+        
         main_window.noutrefresh()
         curses.doupdate()
 
