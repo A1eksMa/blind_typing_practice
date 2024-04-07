7c6d6256da9edc5b92cebc420d8faf3d3c9393c8
root <root@194-67-121-15.cloudvps.regruhosting.ru>
2024-04-07 21:13:15 +0300

    Add palette and path_to_file variable

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from windows.main_window import *
 
 # System settings
 
+# Path to file
+path_name = 'txt'
+
 # Timing
 started_time = None
 current_time = None
@@ -33,10 +36,16 @@ def main(stdscr):
     max_width = curses.COLS - 1
     max_height = curses.LINES - 1
 
-
+    # System palette
     curses.start_color()
-    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLUE)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_YELLOW)
+    curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_BLUE)
+    curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_MAGENTA)
+    curses.init_pair(7, curses.COLOR_BLACK, curses.COLOR_CYAN)
+    curses.init_pair(8, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
     stdscr.clear()
     stdscr.bkgd(' ', curses.color_pair(1))
