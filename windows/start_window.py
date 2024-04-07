ec0cdb7726632681619d6c0c8b08d2a2dcacca90
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 02:05:33 +0600

    Update start_window.py

--- windows/start_window.py
+++ windows/start_window.py
@@ -73,18 +73,15 @@ def start_window(max_height, max_width):
             break
         elif user_input == 39:
             active_button = 2 if active_button == 1 else 1
+            draw_buttons(active_button)
         elif user_input == 37:
             active_button = 1 if active_button == 2 else 2
+            draw_buttons(active_button)
+
+        """
         elif user_input == 13:
             start_win.addstr(0, 0, f"Button {active_button} selected", curses.color_pair(2))
             start_win.refresh()
-
-        draw_buttons(active_button)
-        
-
-
-        
-        """
         else:
             start_win.clear()
             start_win.refresh()
