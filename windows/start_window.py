c3f6a33348986d20dbb62c3387de0b79928202ba
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 01:57:34 +0600

    Update start_window.py

--- windows/start_window.py
+++ windows/start_window.py
@@ -68,14 +68,14 @@ def start_window(max_height, max_width):
     start_win.refresh()
     
     while True:
-        user_input = start_win.getkey()
-        if user_input == curses.KEY_BREAK:
+        user_input = start_win.getch()
+        if user_input == 27:
             break
-        elif user_input == curses.KEY_RIGHT:
+        elif user_input == 39:
             active_button = 2 if active_button == 1 else 1
-        elif user_input == curses.KEY_LEFT:
+        elif user_input == 37:
             active_button = 1 if active_button == 2 else 2
-        elif user_input == curses.KEY_ENTER:
+        elif user_input == 13:
             start_win.addstr(0, 0, f"Button {active_button} selected", curses.color_pair(2))
             start_win.refresh()
 
