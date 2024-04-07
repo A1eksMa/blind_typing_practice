1514cfd97ad40e345c97a53b6d572610065daded
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 03:46:34 +0600

    Update start_window.py

--- windows/start_window.py
+++ windows/start_window.py
@@ -97,7 +97,10 @@ def start_window(max_height, max_width):
         if user_input == 27 or user_input == ord('q'):
             break
         elif user_input == 9:
-            active_button += 1 if active_button <= 2 else active_button = 1
+            if active_button < 2:
+                active_button += 1 
+            else:
+                active_button = 1
         elif user_input == ord('a'):
             active_button = 2 if active_button == 1 else 1
             draw_buttons(active_button)
