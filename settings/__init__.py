17433ebd380e08f37127b9c7ad847109b3b2bb39
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-09 00:12:21 +0600

    Create __init__.py

---
+++ settings/__init__.py
@@ -0,0 +1,32 @@
+# System settings
+class Settings(Preferences):
+    def __init__(self):
+        pass
+
+    def get_display(self):
+        self.height = curses.LINES - 1
+        self.width = curses.COLS - 1
+        return self.height, self.width
+
+# User settings
+class Preferences():
+    def __init__(self):
+        # Path to file
+        self.path_to_file = ''  
+
+# Timing
+started_time = None
+current_time = None
+duration = 0
+
+# Statistics
+typed_keys = 0
+typed_right = 0
+
+speed = 0
+accuracy = 0
+
+# Errors
+errors = 0
+expected_input = None
+user_input = None
