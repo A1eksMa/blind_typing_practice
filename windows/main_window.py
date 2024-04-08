25d7aab699216f867fde9877ce0ae3908552a5f0
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 15:30:16 +0600

    Update main_window.py

--- windows/main_window.py
+++ windows/main_window.py
@@ -8,27 +8,27 @@ def main_window(path_to_file, debug_mode = False):
     max_height = curses.LINES - 1
 
     # Init main window
-    main_window = curses.newwin(max_height,
+    main_win = curses.newwin(max_height,
                               max_width,
                               0,
                               0)
 
-    main_window.bkgd(' ', curses.color_pair(2))
+    main_win.bkgd(' ', curses.color_pair(2))
 
     # Draw rectangle frame
     rectangle_height = max_height - 2
     rectangle_width = max_width - 2
-    rectangle(main_window, 1, 1, rectangle_height, rectangle_width)
+    rectangle(main_win, 1, 1, rectangle_height, rectangle_width)
 
     title = 'Blind Typing Trainer'
-    main_window.addstr(0,
+    main_win.addstr(0,
                      0,
                      title.center(max_width),
                      curses.color_pair(2))
 
-    main_window.refresh()
-    main_window.getch()
-   #main_window.noutrefresh()
+    main_win.refresh()
+    main_win.getch()
+   #main_win.noutrefresh()
    #curses.doupdate()
     if debug_mode:
         main_win.addstr(0, 0, f"Path to file: {path_to_file}", curses.color_pair(2))
