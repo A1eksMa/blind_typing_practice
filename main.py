a504fd44b7d8b0c42f124321ea58cc7fb8c31099
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-09 00:15:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,9 @@
 import curses
 from curses.textpad import rectangle
 from windows import *
-
-
-# System settings
-class Settings():
-    def __init__(self):
-        # Path to file
-        self.path_to_file = ''
-    
-    def get_display(self):
-        self.height = curses.LINES - 1
-        self.width = curses.COLS - 1
-        return self.height, self.width
+from settings import *
              
 user_settings = Settings()
-
-# Timing
-started_time = None
-current_time = None
-duration = 0
-
-# Statistics
-typed_keys = 0
-typed_right = 0
-
-speed = 0
-accuracy = 0
-
-# Errors
-errors = 0
-expected_input = None
-user_input = None
-
 
 def main(stdscr):
 
