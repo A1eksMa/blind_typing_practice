9dea484758117d006da603913f8314930ce91fa2
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 18:10:07 +0600

    Update __init__.py

---
+++ windows/__init__.py
@@ -0,0 +1,7 @@
+    import curses
+    from curses.textpad import rectangle
+    
+    # Curses settings
+    curses.curs_set(True)
+    max_width = curses.COLS - 1
+    max_height = curses.LINES - 1
