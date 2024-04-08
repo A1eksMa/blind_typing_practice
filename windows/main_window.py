d3f0076ab2c270b2917d3b0789ade7fa41cefa51
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 23:32:26 +0600

    Update main_window.py

--- windows/main_window.py
+++ windows/main_window.py
@@ -65,6 +65,6 @@ def main_window(curses, user_settings, debug_mode = False):
                     main_win.addstr(0,0,f"Excpected: {char} Input: {user_input}",curses.color_pair(2))
                     main_win.refresh()
             succesful_input +=char
-            pad.addstr(i, len(typing_list[i][0])+1, succesful_input, curses.color_pair(4))
+            pad.addstr(i, len(typing_list[i][0]), succesful_input, curses.color_pair(4))
             pad.refresh(0, 0, 4, left_padding_pad, max_height, max_width)
 
