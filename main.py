5c0b0e8cf47ebee8082b87f35d6501393c167ba7
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 11:33:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -52,18 +52,6 @@ def main(stdscr):
     stdscr.bkgd(' ', curses.color_pair(1))
     stdscr.refresh()
 
-    def draw_rectangle(win, height, width, start_y, start_x):
-        for y in range(start_y, start_y + height):
-            win.addch(y, start_x, '|')  # Vertical lines
-            win.addch(y, start_x + width - 1, '|')
-        for x in range(start_x, start_x + width):
-            win.addch(start_y, x, '-')  # Horizontal lines
-            win.addch(start_y + height - 1, x, '-')
-
-
-    def exit_window():
-        pass
-
     exit_window(max_height, max_width)
 
 
