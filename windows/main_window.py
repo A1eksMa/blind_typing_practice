93f2f94d8599ba37a2b16c25dbf10003dd9da560
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 19:03:12 +0600

    Update main_window.py

--- windows/main_window.py
+++ windows/main_window.py
@@ -1,9 +1,8 @@
-def main_window(user_settings, debug_mode = False):
+def main_window(curses, user_settings, debug_mode = False):
 
     # Curses settings
     curses.curs_set(True)
-    max_width = curses.COLS - 1
-    max_height = curses.LINES - 1
+    max_height, max_width = user_settings.get_display()
 
     # Init main window
     main_win = curses.newwin(max_height,
