9638cb24631ce0841d35eb0a8468976b11fb6e4a
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 13:23:03 +0600

    Update start_window.py

--- windows/start_window.py
+++ windows/start_window.py
@@ -1,15 +1,17 @@
-def start_window(max_height, max_width):
+def start_window(max_height, max_width, debug_mode = False):
 
     import curses
-    from curses.textpad import Textbox, rectangle
-
+    from curses.textpad import rectangle
     from windows.main_window import main_window
 
-    curses.curs_set(False)
+    # Curses settings
+    curses.curs_set(True)
 
+    # Set width start window
     left_and_right_padding = 12
     start_win_width = max_width - 2*left_and_right_padding
 
+    # Set height start window
     top_padding = 10
     start_win_height = max_height - 2*top_padding
 
@@ -17,8 +19,7 @@ def start_window(max_height, max_width):
     if start_win_height < min_start_win_height:
         start_win_height = min_start_win_heigh
 
-    buttons_row = start_win_height - 5
-
+    # Init start window
     start_win = curses.newwin(start_win_height,
                               start_win_width,
                               top_padding,
@@ -27,11 +28,11 @@ def start_window(max_height, max_width):
     start_win.bkgd(' ', curses.color_pair(5))
     start_win.keypad(True)
 
+    # Content of exit window
     title = 'Welcome to Blind Typing Practice!'
     start_win.addstr(3, 0, title.center(start_win_width))
-    start_win.addstr('\n\n')
-    message = 'Press any key to continue or Esc to exit'
-    start_win.addstr(message.center(start_win_width))
+    message = 'Input path to typing file and press Enter to continue (or Esc to exit)'
+    start_win.addstr(5, 0 ,message.center(start_win_width))
 
     start_win.noutrefresh()
 
@@ -43,77 +44,23 @@ def start_window(max_height, max_width):
     start_win.noutrefresh()
     curses.doupdate()
 
-    # Add two buttons
-    button1 = "  Continue  "
-    button2 = "    Exit    "
-    active_button = 1
-
-    button_space = 8*" "
-    button_padding = int((start_win_width - len(button1 + button_space + button2))/2)*" "
-
-    def draw_buttons(active_button):
-        if active_button == 1:
-            start_win.addstr(buttons_row, 0, button_padding)
-            start_win.addstr(buttons_row, len(button_padding), button1, curses.color_pair(8))
-            start_win.addstr(buttons_row, len(button_padding+button1), button_space)
-            start_win.addstr(buttons_row, len(button_padding+button1+button_space), button2, curses.color_pair(1))
-        elif active_button == 2:
-            start_win.addstr(buttons_row, 0, button_padding)
-            start_win.addstr(buttons_row, len(button_padding), button1, curses.color_pair(1))
-            start_win.addstr(buttons_row, len(button_padding+button1), button_space)
-            start_win.addstr(buttons_row, len(button_padding+button1+button_space), button2, curses.color_pair(8))
-        start_win.refresh()
-    
-    draw_buttons(active_button)
-    
     path_to_file = ''
-
-    
-    def set_file_path(path_to_file):
-        user_input = ''
-        while True:
-            curses.curs_set(True)
-            start_win.clear()
-            start_win.addstr(0, 0, path_to_file)
-            start_win.refresh()
-            user_input = start_win.getch()
-         
-            if user_input in [10,13,curses.KEY_ENTER]:
-                break
-            elif user_input == curses.KEY_BACKSPACE:
-                path_to_file = path_to_file[:-1]
-            else:
-                path_to_file += chr(user_input)
-        start_window(max_height, max_width)
-    
-    set_file_path(path_to_file)
-
-    
-
-    
-    start_win.addstr(0, 0, f"Button {active_button} selected, path to file: {path_to_file}", curses.color_pair(2))
-    start_win.refresh()
     
     while True:
         user_input = start_win.getch()
-        if user_input == 27 or user_input == ord('q'):
+        if user_input == 27:
             break
-        elif user_input == ord('a'):
-            active_button = 2 if active_button == 1 else 1
-            draw_buttons(active_button)
-        elif user_input == ord('d'):
-            active_button = 1 if active_button == 2 else 2
-            draw_buttons(active_button)
+        elif user_input in [10,13,curses.KEY_ENTER]:
+            main_window(max_height, max_width)
+        elif user_input == curses.KEY_BACKSPACE:
+            path_to_file = path_to_file[:-1]
+            start_win.addstr(9, 3, f"Path to file: {path_to_file} ", curses.color_pair(1))
+            start_win.refresh()
+        else:
+            path_to_file += chr(user_input)
+            start_win.addstr(9, 3, f"Path to file: {path_to_file} ", curses.color_pair(1))
+            start_win.refresh()
             
-        start_win.addstr(0, 0, f"Button {active_button} selected", curses.color_pair(2))
-        start_win.refresh()
-        
-        if user_input in [10,13,curses.KEY_ENTER]:
-            if active_button == 1:
-                start_win.clear()
-                start_win.refresh()
-                main_window(max_height, max_width)
-            elif active_button == 2:
-                break
-            
-
+        if debug_mode:
+            start_win.addstr(0, 0, f"Press {chr(user_input)}", curses.color_pair(2))
+            start_win.refresh()
