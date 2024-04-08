b51e7670ab8e107d5b7b2b5279b0df4117af6313
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2024-04-08 12:32:17 +0600

    Update exit_window.py

--- windows/exit_window.py
+++ windows/exit_window.py
@@ -1,15 +1,17 @@
-def exit_window(max_height, max_width):
+def exit_window(max_height, max_width, debug_mode = False):
 
     import curses
-    from curses.textpad import Textbox, rectangle
-
+    from curses.textpad import rectangle
     from windows.main_window import main_window
 
+    # Curses settings
     curses.curs_set(False)
 
+    # Set width exit window
     left_and_right_padding = 12
     exit_win_width = max_width - 2*left_and_right_padding
 
+    # Set height exit window
     top_padding = 10
     exit_win_height = max_height - 2*top_padding
 
@@ -17,8 +19,9 @@ def exit_window(max_height, max_width):
     if exit_win_height < min_exit_win_height:
         exit_win_height = min_exit_win_heigh
 
-    buttons_row = exit_win_height - 5
 
+
+    # Init exit window
     exit_win = curses.newwin( exit_win_height,
                               exit_win_width,
                               top_padding,
@@ -27,9 +30,10 @@ def exit_window(max_height, max_width):
     exit_win.bkgd(' ', curses.color_pair(5))
     exit_win.keypad(True)
 
+    # Content of exit window
     title = 'Statistics:'
     exit_win.addstr(3, 0, title.center(exit_win_width))
-    exit_win.addstr('\n\n')
+    
     message = 'Press Tab to move and Enter to confirm'
     exit_win.addstr(buttons_row-2, 0, message.center(exit_win_width))
 
@@ -48,6 +52,7 @@ def exit_window(max_height, max_width):
     button2 = "    Exit    "
     active_button = 1
 
+    buttons_row = exit_win_height - 5
     button_space = 8*" "
     button_padding = int((exit_win_width - len(button1 + button_space + button2))/2)*" "
 
@@ -65,10 +70,10 @@ def exit_window(max_height, max_width):
         exit_win.refresh()
     
     draw_buttons(active_button)
-    
-   
-    exit_win.addstr(0, 0, f"Button {active_button} selected", curses.color_pair(2))
-    exit_win.refresh()
+
+    if debug_mode:
+        exit_win.addstr(0, 0, f"Button {active_button} selected", curses.color_pair(2))
+        exit_win.refresh()
     
     while True:
         
@@ -87,6 +92,9 @@ def exit_window(max_height, max_width):
             elif active_button == 2: active_button = 1
                 
             draw_buttons(active_button)
-            exit_win.addstr(0, 0, f"Button {active_button} selected", curses.color_pair(2))
             rectangle(exit_win, 1, 1, rectangle_height, rectangle_width)
             exit_win.refresh()
+
+            if debug_mode:
+                exit_win.addstr(0, 0, f"Button {active_button} selected", curses.color_pair(2))
+                exit_win.refresh()
